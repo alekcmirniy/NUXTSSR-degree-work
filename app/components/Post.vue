34e942037e16ee17735a647da22737e1e88b9d62
alekcmirniy <alekcmirniy@bk.ru>
2026-03-09 17:12:20 +0300

    post reorg

--- app/components/Post.vue
+++ app/components/Post.vue
@@ -1,20 +1,20 @@
 <template>
-    <section class="container">
-        <header class="header">
-            <h1 class="header-text">{{ postData.headerText }}</h1>
-            <aside class="header-aside-container">
-                <div class="add-to-favourites">⚝</div>
-                <div class="post-date">{{ "преобразованная дата" }}</div>
+    <section class="flex flex-col">
+        <header class="flex">
+            <h1>{{ postData.headerText }}</h1>
+            <aside class="flex flex-col">
+                <div>⚝</div>
+                <div>{{ "преобразованная дата" }}</div>
             </aside>
         </header>
 
-        <main class="main-content">
+        <main>
             <!-- IMG -->
         </main>
 
-        <footer class="footer">
-            <p class="post-author">Author: {{ postData.author }}</p>
-            <p class="post-epigraph">{{ postData.epigraph }}</p>
+        <footer class="flex flex-col">
+            <p>Author: {{ postData.author }}</p>
+            <p class="text-center">{{ postData.epigraph }}</p>
         </footer>
     </section>
 </template>
@@ -22,37 +22,9 @@
 <script lang="ts" setup>
 import type { PostData } from "../utils/interfaces/posts";
 
-const props = defineProps<{
-    postData: PostData;
-}>();
-const postData = props.postData;
+const { postData } = defineProps<{ postData: PostData }>();
 
 const transformData = () => {
     //преобразование даты под шаблон
 };
 </script>
-
-<style lang="scss" scoped>
-.container {
-    display: flex;
-    flex-direction: column;
-
-    .header {
-        display: flex;
-
-        .header-aside-container {
-            display: flex;
-            flex-direction: column;
-        }
-    }
-
-    .footer {
-        display: flex;
-        flex-direction: column;
-
-        .post-epigraph {
-            text-align: center;
-        }
-    }
-}
-</style>
