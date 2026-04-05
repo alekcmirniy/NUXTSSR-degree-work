a702e3d4607f3d0dc5837f8f94a3e9831cc2e804
alekcmirniy <alekcmirniy1@gmail.com>
2026-04-05 20:41:01 +0300

    форма авторизации начальная версия, надо делать регистрацию

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,4 +1,15 @@
-<template></template>
+<template>
+    <div>Вход</div>
+    <form class="flex flex-col gap-4 justify-center items-center">
+        <label for="email">Почта</label>
+        <input name="email" v-model="email" />
+
+        <label for="password">Пароль</label>
+        <input name="password" v-model="password" />
+
+        <button @click="login">Войти</button>
+    </form>
+</template>
 
 <script setup>
 const email = ref("");
